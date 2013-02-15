b89bc63baacb18ef9c289e9309896b03a7c7e759
Brian Pitts <brian@polibyte.com>
2013-02-15 14:45:49 -0600

    Delete output after running test

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -2,3 +2,4 @@
 s3crets -s input/secrets -j input/
 echo "\nShowing expected output on the left and actual output on the right\n"
 diff -y expected_output.json input/node.new.json 
+rm input/node.new.json
